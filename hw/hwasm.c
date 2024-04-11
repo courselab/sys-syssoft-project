b8548865e68a91d08d562a9b9552747399485aed
Matheus Pereira Dias <matheuspd07@gmail.com>
2024-04-11 16:38:46 -0300

    Initial commit

---
+++ hw/hwasm.c
@@ -0,0 +1,7 @@
+/*
+ *    SPDX-FileCopyrightText: 2024 Matheus Pereira Dias <matheuspd07@gmail.com>
+ *   
+ *    SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *    This file is part of SYSeg, available at https://gitlab.com/monaco/syseg.
+ */
